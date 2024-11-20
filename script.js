fe0f0d2fbb68a207c2d56023161ca857cd57e6d4
Soul-srs <167603950+Soul-srs@users.noreply.github.com>
2024-11-20 09:23:34 -0300

    Version 1.1

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Hello
+const ntc = document.getElementById('notch-container');
+const nt = document.getElementById('notch');
+
+nt.onmouseover = () => {
+    nt.style.width = '85%';
+    nt.style.height = "200%";
+    nt.style.borderRadius = '80px';
+}
+
+nt.onmouseout = () => {
+    nt.style.width = '85%';
+    nt.style.height = '60%';
+}
